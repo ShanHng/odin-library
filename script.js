8d54810f2c98c57852a1ae87668078f10f21a8d1
ShanHng <hngshanhern@gmail.com>
2023-07-15 11:52:58 +0800

    Add Book constructor and addBookToLibrary function

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
-let myLibrary = [];
+let myLibrary = []
 
-function Book() {
+function Book (title, author, noOfPages) {
   // the constructor...
+  this.title = title
+  this.author = author
+  this.noOfPages = noOfPages
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary (...books) {
   // do stuff here
-  myLibrary.push(book);
+  for (const book of books) {
+    myLibrary.push(book)
+  }
 }
 
 // main program logic flow
-addBookToLibrary({
-    title:"The Country of Floptropica",
-    author: "Pooja",
-    noOfPages: 19,
-});
+addBookToLibrary(
+  new Book('The Country of Floptropica', 'Pooja', 19),
+  new Book('The Country of Floptropica', 'Pooja', 19),
+  new Book('The Country of Floptropica', 'Pooja', 19)
+)
 
 // DOM manipulation below
-const booksContainer = document.getElementsByClassName("books-container");
+const booksContainer = document.getElementsByClassName('books-container')
 
 for (const book of myLibrary) {
-    const bookCard = document.createElement("div");
-    const bookTitle = document.createElement("div");
-    const bookAuthor = document.createElement("div");
-    const bookNoOfPages = document.createElement("div");
+  const bookCard = document.createElement('div')
+  const bookTitle = document.createElement('div')
+  const bookAuthor = document.createElement('div')
+  const bookNoOfPages = document.createElement('div')
 
-    bookTitle.textContent = book.title;
-    bookAuthor.textContent = "Author: " + book.author;
-    bookNoOfPages.textContent = "No of Pages: " + book.noOfPages;
+  bookTitle.textContent = book.title
+  bookAuthor.textContent = 'Author: ' + book.author
+  bookNoOfPages.textContent = 'No of Pages: ' + book.noOfPages
 
-    bookTitle.setAttribute("style", "border-bottom: 1px solid grey;" + "padding-bottom: 1rem;" + "font-weight: bold;");
-    // solution to add new line via textContent obtained from 
-    // https://stackoverflow.com/questions/9980416/how-can-i-insert-new-line-carriage-returns-into-an-element-textcontent
-    bookAuthor.setAttribute("style", "white-space: pre-line;" + "padding-top: 1rem;");
-    bookNoOfPages.setAttribute("style", "white-space: pre-line;");
+  bookTitle.setAttribute(
+    'style',
+    'border-bottom: 1px solid grey;' +
+      'padding-bottom: 1rem;' +
+      'font-weight: bold;'
+  )
+  // solution to add new line via textContent obtained from
+  // https://stackoverflow.com/questions/9980416/how-can-i-insert-new-line-carriage-returns-into-an-element-textcontent
+  bookAuthor.setAttribute(
+    'style',
+    'white-space: pre-line;' + 'padding-top: 1rem;'
+  )
+  bookNoOfPages.setAttribute('style', 'white-space: pre-line;')
 
-    bookCard.className = "book-card";
-    bookCard.append(bookTitle, bookAuthor, bookNoOfPages);
+  bookCard.className = 'book-card'
+  bookCard.append(bookTitle, bookAuthor, bookNoOfPages)
 
-    booksContainer[0].appendChild(bookCard);
-}   
+  booksContainer[0].appendChild(bookCard)
+}
 
-const newBookForm = document.querySelector("#new-book-form");
-newBookForm.addEventListener("submit", event => event.preventDefault());
+const newBookForm = document.querySelector('#new-book-form')
+newBookForm.addEventListener('submit', event => event.preventDefault())
 
-const addBookBtn = document.querySelector(".add-book-button");
-addBookBtn.addEventListener('click', e => newBookForm.classList.remove("hidden"));
+const addBookBtn = document.querySelector('.add-book-button')
+addBookBtn.addEventListener('click', e =>
+  newBookForm.classList.remove('hidden')
+)
 
-const closeFormBtn = document.querySelector(".form-close-btn");
-closeFormBtn.addEventListener('click', () => newBookForm.classList.add("hidden"));
+const closeFormBtn = document.querySelector('.form-close-btn')
+closeFormBtn.addEventListener('click', () =>
+  newBookForm.classList.add('hidden')
+)
