897d15430d557be281c86224bdd974ebfab9f6a2
ShanHng <hngshanhern@gmail.com>
2023-07-15 11:48:09 +0800

    Add form to add new book

--- script.js
+++ script.js
@@ -30,6 +30,8 @@ for (const book of myLibrary) {
     bookNoOfPages.textContent = "No of Pages: " + book.noOfPages;
 
     bookTitle.setAttribute("style", "border-bottom: 1px solid grey;" + "padding-bottom: 1rem;" + "font-weight: bold;");
+    // solution to add new line via textContent obtained from 
+    // https://stackoverflow.com/questions/9980416/how-can-i-insert-new-line-carriage-returns-into-an-element-textcontent
     bookAuthor.setAttribute("style", "white-space: pre-line;" + "padding-top: 1rem;");
     bookNoOfPages.setAttribute("style", "white-space: pre-line;");
 
@@ -38,3 +40,12 @@ for (const book of myLibrary) {
 
     booksContainer[0].appendChild(bookCard);
 }   
+
+const newBookForm = document.querySelector("#new-book-form");
+newBookForm.addEventListener("submit", event => event.preventDefault());
+
+const addBookBtn = document.querySelector(".add-book-button");
+addBookBtn.addEventListener('click', e => newBookForm.classList.remove("hidden"));
+
+const closeFormBtn = document.querySelector(".form-close-btn");
+closeFormBtn.addEventListener('click', () => newBookForm.classList.add("hidden"));
